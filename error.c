5b3dc97c8147819bbf586cb778e6592daf96f488
kompass <kirchner.nic@gmail.com>
2014-04-01 00:27:23 +0200

    Début système de gestion des erreurs

---
+++ error.c
@@ -0,0 +1,33 @@
+#include "error.h"
+
+error_t g_error_list = NULL;
+
+void new_error(int error_type, char *comment)
+{
+	error_t error = malloc(sizeof(error_t));
+
+	if(error == NULL)
+	{
+		fprintf(stderr,
+			"Erreur fatale : Erreur d'allocation mémoire dans le système de gestion d'erreur.\n");
+
+		exit(1);
+	}
+
+	error->type = error_type;
+	error->comment = malloc((strlen(comment)+1)*sizeof(char));
+
+	if(error->comment = NULL)
+	{
+		free(error);
+		fprintf(stderr,
+			"Erreur fatale : Erreur d'allocation mémoire dans le système de gestion d'erreur.\n");
+
+		exit(1);
+	}
+
+	strcpy(error->comment, comment);
+
+	error->next = g_error_list;
+	g_error_list = error;
+}
---
+++ error.h
@@ -0,0 +1,18 @@
+#ifndef ERROR_KGDFLOW_HEADER
+#define ERROR_KGDFLOW_HEADER
+
+typedef struct error
+{
+	int type;
+	char *comment;
+	struct error *next;
+	
+} error_t;
+
+extern error_t g_error_list;
+
+void new_error(int error_type, char *comment);
+
+
+
+#endif
